7e57e0d0f528d20614fc3cd0c704e835a2b09c7c
rookiegrammer <30749394+rookiegrammer@users.noreply.github.com>
2018-11-28 12:48:23 +0800

    code main

---
+++ code/mainline/Group.m
@@ -0,0 +1,12 @@
+classdef Group < handle
+    properties
+        type
+        content
+        fcount = 0 %female count
+        lbest % local best
+        lbestval % local best value
+    end
+    methods
+        
+    end
+end
